d2479e6d721e704339f004d0c6f3323934b3ea50
malinjr07 <malinjr07@gmail.com>
2023-10-03 18:55:05 +0600

    js init

---
+++ arrays/problem-1431.js
@@ -0,0 +1,9 @@
+var numJewelsInStones = function (s, k) {
+  const sArr = s.split(' ');
+  const rsltArr = sArr.splice(0, k);
+};
+
+numJewelsInStones('What is the solution to this problem', 4);
+/* smallerNumbersThanCurrent([6, 5, 4, 8]);
+smallerNumbersThanCurrent([7, 7, 7, 7]); */
+
