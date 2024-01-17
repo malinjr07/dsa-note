a465a140b897393e952a154db76df87b02bdc78b
malinjr07 <malinjr07@gmail.com>
2024-01-17 09:48:42 +0600

    non constructible change

--- problems/algoExpert/nonConstructibleChange.js
+++ problems/algoExpert/nonConstructibleChange.js
@@ -1,9 +1,22 @@
+/**
+ * The base principle is,
+ * in a sorted array,
+ * one element must have it's 1 integer larger value on the next element,
+ * if we don't find that element, then that is the non-constructive change
+ * even if all the elements are in sequence, if we sum up all integer,
+ * that'd be the last change amount that those element can create altogether
+ * And, if we add 1 with that sum value, we will get the lowest non-constructive change of the given coins
+ */
+
 function nonConstructibleChange(coins) {
   // Write your code here.
-  coins.sort((a, b) => a - b);
   let currentCreatedChange = 0;
+  console.log('coins:', coins);
+  coins.sort((a, b) => a - b);
+  console.log('coins:', coins);
 
   for (let coin of coins) {
+    console.log('coin:', coin, ', currentCreatedChange:', currentCreatedChange);
     if (coin > currentCreatedChange + 1) {
       return console.log(currentCreatedChange + 1);
     }
@@ -11,3 +24,5 @@ function nonConstructibleChange(coins) {
   }
   return console.log(currentCreatedChange + 1);
 }
+
+nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]);
