0ad4fa405e41af12a62cad252ed4d6d9b5517d43
shounakraj <shounakraj@gmail.com>
2024-02-24 09:30:50 +0600

    Majority Count

--- 01. array/problem-169.js
+++ 01. array/problem-169.js
@@ -1,3 +1,11 @@
+/**
+ * # Majority Element #
+ * Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ * ## Thought Process | Time O(N) Space O(N) ##
+ * We will take an object (HashMap) and keep adding the value as key and the number of occurance as value. Also, we will keep track of the number of maximum occurance in one variable and the value that is correspondent with that number in another value.
+ * We will iterate throughout the array. On each iteration, we will check if the value exist in the object or not. If it exist, we will increase its count and then we will check if the count of current element is greater than the current maxCount. If it's greater, we will update the maxCount and the Major Number as well. If the value doesn't exist in the object, we will create a property with default value of 1. Finally, we we will return the Major Number
+ */
+
 var majorityEl = function (nums) {
   let countObj = {};
   let maxCount = 0;
@@ -18,6 +26,11 @@ var majorityEl = function (nums) {
 };
 
 console.log(majorityEl([6, 5, 5]));
+
+/**
+ * ### Optimal Solution | Time O(N) Space O(1) ###
+ * We need one major number, which happen to be have maximum occurance. So, if one number has 4 occurance, where another has 5 occurance, the 5 occurance will have the majority. So, to determine the majority, we can take two pointer, one will point at the element while other will count the element's majority. We will traverse the element and check if the current element is matching with our major element or not. If it matches, we will increase the count. If it doesn't match, we will decrease the count. If, for any reason, the count of the current major element hits the 0, that mean there is another element with equal number of quantity. So, we will update the count value to our current iterating value
+ */
 
 function majorityElement(nums) {
   let candidate;
