b95deca8283b50cf18228056389ad64afa8908f3
malinjr07 <malinjr07@gmail.com>
2025-02-06 20:31:09 +0600

    clean slate

--- 02. string/index.js
+++
@@ -1,8 +0,0 @@
-var isPalindrome = function (s) {
-  const onlyChars = s.replaceAll(/[^a-zA-Z0-9]/gm, '').toLowerCase();
-  console.log('🚀 ~ isPalindrome ~ onlyChars:', onlyChars);
-
-  return onlyChars === onlyChars.split('').reverse().join('');
-};
-
-console.log(isPalindrome('0P'));
